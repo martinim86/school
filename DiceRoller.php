7c3353a69af91eb9841fe43458292a4d736d96e1
Ivan <vano4461@gmail.com>
2017-11-08 10:31:37 +0700

    DiceRoll 08/11/2017

--- DiceRoller.php
+++ DiceRoller.php
@@ -1,70 +1,133 @@
 <?php
 
 /**
- * Class DiceRoller
+ * Class DiceRoller generates throwing dice
  *
  * https://gist.github.com/kroer/aec66e84ff4d63fa7c46a5eeb65ac958
  */
 class DiceRoller {
-    /*массив значений, выпавших на костях*/
     /**
      * @var $diceValues = [];
      */
     protected $diceValues = [];
-	/*количество костей*/
+    /**
+     * @var $diceCount
+     */
 	private $diceCount;
-    /*конструктор. Считать что на всех костях изначально выпала шестёрка*/
-	function __construct($count) {
+    /**
+     * DiceRoller constructor.
+     *
+     * Generate values in throwing dice.
+     * To consider that on all dices initially six fell out.
+     *
+     * @param $count
+     */
+    function __construct($count) {
         for ($i = 1; $i <= $count; $i++) {
             $this->diceValues[$i] = 6;
         }
+        $this->diceCount = $count;
     }
-    /*возвращает количество костей*/
-	public function getCount(){
+    /**
+     * return count of  dice;
+     *
+     * @return $this->diceCount
+     */
+    public function getCount(){
 		return $this->diceCount;
     }
-    /*возвращает значение на кости с указанныи индексом*/
-	public function getValue($index)
+    /**
+     * Return dice value with index
+     *
+     * @param $index
+     * @return string
+     */
+    public function getValue($index)
     {
-		return array_pop($this->diceValues);
+        $value = $this->diceValues[$index];
+        return "Индекс кубика: ".$index."<br> Значение: ".$value;
     }
-    /*бросаает кость с указанным индексом*/
-	public function roll($index)
+    /**
+     * Throw dice value with index
+     *
+     * @param $index
+     */
+    public function roll($index)
     {
         $this->diceValues[$index]=mt_rand(1,6);
     }
-    /*возвращает сумму значений на всех костях*/
-	public function total()
+    /**
+     * Return sum in all dices
+     *
+     * @return int
+     */
+    public function total()
     {
 		return array_sum($this->diceValues);
     }
-    /*возаращает значения всех костей в виде строки*/
-	public function __toString()
+    /**
+     * Returm the values ​​of all dices as a string
+     *
+     * @return string
+     */
+    public function __toString()
     {
         return  "&ldquo; {".implode(",", $this->diceValues)."} &rdquo;";
     }
 }
 
-
+/**
+ * Class RiggedDiceRoller
+ *
+ * should allow the player to cheat
+ */
 class RiggedDiceRoller extends DiceRoller {
-    /*минимальное значение для результата броска кости*/
+    /**
+     * Minimum value
+     *
+     * @var $minValue;
+     */
 	private $minValue;
 
-	/*значение для костей по-умолчанию */
+    /**
+     * Overwrites the parent constructor
+     * RiggedDiceRoller constructor.
+     * @param $count
+     * @param $min
+     * @throws Exception
+     */
     function __construct($count, $min)
     {
-        parent::__construct($count);
-        $this->minValue = $min;
-	}
-	/*возвращает минимальное значение для результата броска кости*/
-	public function getMin()
+
+        if($min >= 1 && $min <= 6){
+            parent::__construct($count);
+            $this->minValue = $min;
+        } else {
+            throw new Exception('Uncorrect minimum ');
+        }
+    }
+    /**
+     * Return a minimum value
+     * @return mixed
+     */
+    public function getMin()
     {
         return $this->minValue;
 	}
+
+    /**
+     * Return a sum of all dices +1
+     * @return float|int
+     */
     public function total()
     {
         return array_sum($this->diceValues)+1;
     }
+
+    /**
+     * Return string ex.  "rigged {4, 3, 6, 5} min 2"
+     * @return string
+     */
     public function __toString()
     {
         return  "rigged &ldquo; {".implode(",", $this->diceValues)."} &rdquo; min ".$this->minValue;
@@ -73,8 +136,11 @@ class RiggedDiceRoller extends DiceRoller {
 }
 
 
-$obj= new RiggedDiceRoller(3,4);
-echo $obj;
 
-
-//?>
+try {
+    $obj= new RiggedDiceRoller(3,4);
+    echo $obj->getCount();
+} catch( Exception $e ) {
+    echo "Error : {$e->getMessage()}";
+}
+?>
