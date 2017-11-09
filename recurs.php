e7cba871a734ba0029ce484e31c6aba75a4cd205
Ivan <vano4461@gmail.com>
2017-11-09 11:09:14 +0700

    recurs.php 09/11/2017

--- recurs.php
+++ recurs.php
@@ -1,13 +1,13 @@
 <?php
+$a = $argv[1];
+$b = $argv[2];
 function go($a,$b) {
-	if($a===$b)return 1;
+	if($a===$b)return $b;
 	if($a < $b){
 		return $a.",".go($a+1,$b);
 	}elseif($a > $b) {
 		return $a.",".go($a-1,$b);
-	} else {
-		return "error";
-	}
+	} 
 }
-echo go(6,1);
+
 ?>
